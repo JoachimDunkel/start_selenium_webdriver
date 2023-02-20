0494812e23be7cf924a0b6c4e56613811adf8693
Joachim Dunkel <joachim.github@gmx.at>
2023-02-20 14:40:48 +0100

    setup to be a proper pypi package

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Example:
 
 ```python
 
-from webdriver_startup import start_web_driver
+from start_selenium_webdriver.webdriver_startup import start_web_driver
 
 end_point = "https://www.google.com/"
 driver = start_web_driver(end_point, num_sec_implicit_wait=0)
--- test/simple_test.py
+++ test/simple_test.py
@@ -1,6 +1,6 @@
-from webdriver_startup import start_web_driver
+from start_selenium_webdriver.webdriver_startup import start_web_driver
 
 end_point = "https://www.google.com/"
 driver = start_web_driver(end_point, num_sec_implicit_wait=0)
 
-print("Done")
+print("DOne")
